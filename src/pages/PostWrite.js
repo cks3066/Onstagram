a7943e7159bdd3579a6039203a27309740b47281
On <cks3066@naver.com>
2021-08-19 23:50:06 +0900

    add moremore

--- src/pages/PostWrite.js
+++ src/pages/PostWrite.js
@@ -7,6 +7,8 @@ import { actionCreators as postActions } from "../redux/modules/post";
 const PostWrite = (props) => {
  const dispatch = useDispatch();
   const is_login = useSelector((state) => state.user.is_login);
+  const preview = useSelector((state) => state.image.preview);
+  
   const { history } = props;
 
   const [contents, setContents] = React.useState();
@@ -44,7 +46,7 @@ const PostWrite = (props) => {
               <Text margin="0px" size="24px" bold>미리보기</Text>
           </Grid>
 
-          <Image shape="rectangle"/>
+          <Image shape="rectangle" src={preview? preview: "https://via.placeholder.com/400x300"}/>
       </Grid>
 
       <Grid padding="16px">
--- src/shared/Upload.js
+++ src/shared/Upload.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { Button } from "../elements";
+import { storage } from "./Firebase";
+import { useDispatch, useSelector } from 'react-redux';
+import { actionCreators as imageActions } from "../redux/modules/image";
 
 const Upload = (props) => {
+  const dispatch = useDispatch();
+  const is_uploading = useSelector((state) => state.image.uploading);
+  const fileInput = React.useRef();
+
+  const selectFile = (e) => {
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.files);
+
+    console.log(fileInput.current.files[0]);
+  
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      console.log(reader.result);
+      dispatch(imageActions.setPreview(reader.result));
+    }
+  };
+
+  const uploadFB = () => {
+    if (!fileInput.current || fileInput.current.files.length === 0) {
+      window.alert("파일을 선택해주세요!");
+      return;
+    }
+    
+    let image = fileInput.current.files[0];
+    dispatch(imageActions.uploadImageFB(image));
+  }
   return (
     <React.Fragment>
-      <input type="file"></input>
+      <input type="file" onChange={selectFile} ref={fileInput} disabled={is_uploading}/>
+      <Button _onClick={uploadFB}>업로드 하기</Button>
     </React.Fragment>
   );
 };
