dc3ad9d0a502bb64798c37427028d79fd5292fc3
On <cks3066@naver.com>
2021-08-20 04:30:35 +0900

    네시반 내시간

--- src/components/Post.js
+++ src/components/Post.js
@@ -19,7 +19,7 @@ const Post = (props) => {
           <Text>{props.contents}</Text>
         </Grid>
         <Grid>
-          <Image shape="rectangle" src={props.src}></Image>
+          <Image shape="rectangle" src={props.image_url}></Image>
         </Grid>
         <Grid padding="16px">
           <Text margin="0px" bold>댓글 {props.comment_cnt}개</Text>
